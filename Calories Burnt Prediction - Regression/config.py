208fef4934399e586b34c5b8b8c15991850a18d9
Sarvesh0811 <116961678+Sarvesh0811@users.noreply.github.com>
2025-02-19 22:46:55 +0530

    Add files via upload

---
+++ Calories Burnt Prediction - Regression/config.py
@@ -0,0 +1,9 @@
+FLASK_PORT_NUMBER = 5050
+
+CSV_FILE_NAME = r"datasets\final_exercise.csv"
+
+COLUMN_DATA_JSON = r"artifacts\column_data.json"
+
+MODEL_FILE_NAME = r"artifacts\rf_model.pkl"
+
+SCALING_FILE_NAME = r"artifacts\std_scaler.pkl"
